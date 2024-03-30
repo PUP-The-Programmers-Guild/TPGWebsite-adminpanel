b2767557c69525a2b56f61bbe00caceb812126a1
Yuan <72153098+Karmotrine@users.noreply.github.com>
2024-03-30 23:46:03 +0800

    feat: login/logout, faq add, update, remove funct.  (#6)
    - feat (next.config.js): add url proxy for serverless api via 'rewrite' option
    - feat (DashboardHeader.tsx): add logout button functionality
    - feat (FaqsAddForm.tsx): set up functionality of adding FAQ thru form
    - feat (FaqsRemoveDialog.tsx): set up functionality of removing FAQ(s) thru dialog
    - feat (FaqsTable.interface.ts): set up interface for FaqsTable component
    - feat (FaqsTable.tsx, FaqsUpdateDialog.tsx, FaqsRemoveDialog.tsx): move update/remove dialog to separate UI components
    - feat (FaqsTable.tsx, FaqsUpdateDialog.tsx, FaqsRemoveDialog.tsx): move dialog states to child UI components
    - feat (FaqsTable.tsx): optimize determining active CRUD buttons thru useMemo
    - feat (LoginForm.tsx): set up login functionality connected to backend
    - feat (add.tsx): add route callback redirect to FAQ data table on success add

--- src/pages/dashboard/faqs/add.tsx
+++ src/pages/dashboard/faqs/add.tsx
@@ -1,10 +1,12 @@
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
 import FaqsAddForm from "@/components/dashboard/faqs/FaqsAddForm";
+import { useRouter } from "next/router";
 
 export default function AddFaqs() {
+    const router = useRouter();
     return (
         <DashboardLayout>
-            <FaqsAddForm />
+            <FaqsAddForm routeSuccessCallback={() => router.push('/dashboard/faqs')}/>
         </DashboardLayout>
     )
 }
